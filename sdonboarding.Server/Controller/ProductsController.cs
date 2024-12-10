07f21128948a84bf95eed4799ff937b27f8d60a3
Krutika Patel <patelkrutika181@gmail.com>
2024-12-10 21:57:36 +1100

    Onboarding Task-Product Crud Operation

---
+++ sdonboarding.Server/Controller/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sdonboarding.Server.Dtos;
+using sdonboarding.Server.Mappers;
+using sdonboarding.Server.Models;
+
+namespace sdonboarding.Server.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly OnBoardingProjectContext _context;
+
+        public ProductsController(OnBoardingProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+        {
+            var _products = await _context.Products.ToListAsync();
+            if (_products.Count > 0)
+            {
+                return Ok(_products);
+            }
+            else
+            {
+                return BadRequest("There are no customer");
+            }
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return ProductMapper.EntitytoDto(product);
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, ProductDto productDto)
+        {            // Validate if the ID in the URL matches the ID in the DTO
+
+            if (id != productDto.Id)
+
+            {
+
+                return BadRequest("Product ID mismatch.");
+
+            }
+
+            // Check if the product exists in the database
+
+            var existingProduct = await _context.Products.FindAsync(id);
+
+            if (existingProduct == null)
+
+            {
+                return NotFound("Product not found.");
+
+            }
+
+            // Map the DTO to the existing entity
+
+            ProductMapper.UpdateEntityFromDto(existingProduct, productDto);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(ProductDto product)
+        {
+            var entity = ProductMapper.DtotoEntity(product);
+
+            _context.Products.Add(entity);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, ProductMapper.EntitytoDto(entity));
+            
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
