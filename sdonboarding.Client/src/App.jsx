69bbe8745b0ac0be6a933e1ba7ff1b8e9c07f618
Krutika Patel <patelkrutika181@gmail.com>
2024-12-12 15:11:17 +1100

    Onboarding Task - Create Navbar

--- sdonboarding.Client/src/App.jsx
+++ sdonboarding.Client/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 import { useEffect, useState, Component } from 'react';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+//import Footer from "./components/Footer";
 function App() {
   return (
-    <>
-      <h1>MVP Studio Onboarding</h1>
-    </>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route>
+      </Route>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+
   )
 }
 
