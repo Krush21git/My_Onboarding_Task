c61de57e32c85c1901d99c8dd496ec713bf6bf75
Krutika Patel <155936999+Krush21git@users.noreply.github.com>
2025-01-16 00:01:04 +1100

    Update vite.config.js

--- sdonboarding.Client/vite.config.js
+++ sdonboarding.Client/vite.config.js
@@ -1,50 +1,46 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import fs from 'fs';
+import { env } from 'process';
 
-// Dynamically load environment variables based on the mode
-export default defineConfig(({ mode }) => {
-  // Load environment variables from `.env` files
-  const env = loadEnv(mode, process.cwd(), '');
+// Determine the API base URL based on the environment
+const target =
+  env.NODE_ENV === 'production'
+    ? 'https://onboardingcrudoperation-d7ggg0e9ajagdsbp.australiaeast-01.azurewebsites.net' // Your Azure backend URL
+    : 'https://localhost:5001'; // For Local development process
 
-  // Use the environment variable for the API base URL
-  const apiBaseUrl = env.VITE_API_BASE_URL;
-
-  return {
-    plugins: [plugin()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [plugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+  server: {
+    proxy: {
+      '/api/Customers': {
+        target,
+        secure: false,
+      },
+      '/api/Products': {
+        target,
+        secure: false,
+      },
+      '/api/Stores': {
+        target,
+        secure: false,
+      },
+      '/api/Sales': {
+        target,
+        secure: false,
       },
     },
-    build: {
-      outDir: 'dist', // Default output directory
-    },
-    server: {
-      proxy: {
-        '/api/Customers': {
-          target: apiBaseUrl,
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/Products': {
-          target: apiBaseUrl,
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/Stores': {
-          target: apiBaseUrl,
-          changeOrigin: true,
-          secure: false,
-        },
-        '/api/Sales': {
-          target: apiBaseUrl,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-      port: 5173,
-      https: false,
-    },
-  };
+    port: 5173,
+    https: false,
+  },
 });
